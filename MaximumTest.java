76e0d4d9fc63de830e978ac08b17c47b14563772
shraazp <shravyap99@gmail.com>
2021-08-25 21:39:33 +0530

    added:UseCase4 more than three parameters

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,23 +1,27 @@
 package genrics;
-//UseCase 1 given 3 integers ew have to find the maximum
+//UseCase 4 to add more parameters in the maximum function 3 and 4 are used
 
 public class MaximumTest<T extends Comparable<T>> {
 	
 	//parameterized construtor
-	T x,y,z;  //class variables
+	T x,y,z,w;  //class variables
 	
-	//parameterized construuctors
+	//parameterized construuctors for 3 variables
 	public MaximumTest(T x,T y,T z) {
 		this.x=x;
 		this.y=y;
 		this.z=z;
 	}
-	//to get the maximum value
-	 public T maximum()
-	 {
-		 return MaximumTest.maximum(x, y, z);
-	 }
-	//compare to function
+	//parameterized construuctors for 4 variables
+	public MaximumTest(T x,T y,T z,T w) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.w=w;
+	}
+	
+	
+	//compare to function for 3 parameters
 	 public static <T extends Comparable<T>> T maximum(T x,T y,T z) {
 			T max=x;     //will assume x is maximum then compare with y and z
 			if(y.compareTo(max)>0) {
@@ -29,11 +33,30 @@ public class MaximumTest<T extends Comparable<T>> {
 			printMax(x,y,z,max);
 			return max;
 		}
-//to print the maximum value
+	 	//compare to function for 4 parameters
+		 public static <T extends Comparable<T>> T maximum(T x,T y,T z,T w) {
+				T max=x;     //will assume x is maximum then compare with y and z
+				if(y.compareTo(max)>0) {
+					max=y;
+				}
+				if(z.compareTo(max)>0) {
+					max=z;
+				}
+				if(w.compareTo(max)>0)
+				{
+					max=w;
+				}
+				printMax(x,y,z,w,max);
+				return max;
+			}
+	//to print the maximum value for 3 parameters
 	public static <T> void printMax(T x,T y,T z,T max)
 	{
 		System.out.printf("Max of %s,%s and %s is %s\n",x,y,z,max);
 	}
-	
-	
+	//to print the maximum value for 4 parameters
+	public static <T> void printMax(T x,T y,T z,T w,T max)
+	{
+		System.out.printf("Max of %s,%s,%s and %s is %s\n",x,y,z,w,max);
+	}
 }
