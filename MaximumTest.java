fd07e2368052564a6e0dcc790ef85d81e634e5d4
shraazp <shravyap99@gmail.com>
2021-08-25 20:17:33 +0530

    refactored:grneric method is used to find maximum

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,25 +1,25 @@
 package genrics;
 //UseCase 3 given 3 String values we have to find the maximum
 
-public class MaximumTest<String extends Comparable<String>> {
+public class MaximumTest<T extends Comparable<T>> {
 	
 	//parameterized construtor
-	String x,y,z;  //class variables
+	T x,y,z;  //class variables
 	
 	//parameterized construuctors
-	public MaximumTest(String x,String y,String z) {
+	public MaximumTest(T x,T y,T z) {
 		this.x=x;
 		this.y=y;
 		this.z=z;
 	}
 	//to get the maximum value
-	 public String maximum()
+	 public T maximum()
 	 {
 		 return MaximumTest.maximum(x, y, z);
 	 }
 	//compare to function
-	public static <String extends Comparable<String>> String maximum(String x,String y,String z) {
-		String max=x;     //will assume x is maximum then compare with y and z
+	public static <T extends Comparable<T>> T maximum(T x,T y,T z) {
+		T max=x;     //will assume x is maximum then compare with y and z
 		if(y.compareTo(max)>0) {
 			max=y;
 		}
@@ -30,7 +30,7 @@ public class MaximumTest<String extends Comparable<String>> {
 		return max;
 	}
 //to print the maximum value
-	public static <String> void printMax(String x,String y,String z,String max)
+	public static <T> void printMax(T x,T y,T z,T max)
 	{
 		System.out.printf("Max of %s,%s and %s is %s\n",x,y,z,max);
 	}
