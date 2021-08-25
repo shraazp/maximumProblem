4bba9a7d69cca50704a9a4f2acd72a326ccd4efa
shraazp <shravyap99@gmail.com>
2021-08-25 20:54:20 +0530

    refactored usecase1 generic method is added

--- TestCase1.java
+++ TestCase1.java
@@ -4,9 +4,9 @@ public class TestCase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String xStr="pear",yStr="apple",zStr="orange";
-		new MaximumTest(xStr,yStr,zStr).maximum();
 		
+		int xInt=800,yInt=50,zInt=756;
+		new MaximumTest(xInt,yInt,zInt).maximum();
 	}
 
 }
