cebce09b8e696dca76cd8fe923ebe7cc7e6819df
shraazp <shravyap99@gmail.com>
2021-08-25 16:49:26 +0530

    added:UseCase1 maximum for integers

---
+++ TestCase3.java
@@ -0,0 +1,11 @@
+package genrics;
+
+public class TestCase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int xInt=80,yInt=50,zInt=756;
+		new MaximumTest(xInt,yInt,zInt).maximum();
+	}
+
+}
