29915fc3a897f0d5dbf944e91902baf488229052
shraazp <shravyap99@gmail.com>
2021-08-25 20:52:01 +0530

    refactored usecase1 generic method is added

--- TestCase2.java
+++ TestCase2.java
@@ -4,11 +4,10 @@ public class TestCase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//for string values
-		String xStr="apple",yStr="pear",zStr="orange";
-		new MaximumTest(xStr,yStr,zStr).maximum();
 		
-		
+		//for int values
+		int xInt=8,yInt=50,zInt=7;
+		new MaximumTest(xInt,yInt,zInt).maximum();
 	}
 
 }
--- TestCase3.java
+++ TestCase3.java
@@ -3,11 +3,10 @@ package genrics;
 public class TestCase3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//for string values
-		String xStr="orange",yStr="apple",zStr="pear";
-		new MaximumTest(xStr,yStr,zStr).maximum();
 		
+	//for int values
+		int xInt=80,yInt=50,zInt=756;
+		new MaximumTest(xInt,yInt,zInt).maximum();
 	}
 
 }
